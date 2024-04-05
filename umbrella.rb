f7ae311f27a2d4d9635be84d5aae8b586a676b08
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-04-05 18:55:09 +0000

    Geocoding part working!

--- umbrella.rb
+++ umbrella.rb
@@ -1,3 +1,32 @@
+pp "Where are you located?"
+
+user_location = "Chicago"
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+first_result = results.at(0)
+
+geo = first_result.fetch("geometry")
+
+loc = geo.fetch("location")
+
+pp latitude = loc.fetch("lat")
+pp longtude = loc.fetch("lng")
+
 require "http"
 require "json"
 
